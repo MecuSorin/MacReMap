97b0b00b36efe51095ca34859d20cf5e8cc954bd
MECU Sorin <mecumihaisorin@gmail.com>
2019-03-28 21:48:46 +0200

    Remapping Caps>Backspace>Delete>Caps for Mac

---
+++ remapKeys.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x70000002A},{"HIDKeyboardModifierMappingSrc":0x70000002A,"HIDKeyboardModifierMappingDst":0x70000004C},{"HIDKeyboardModifierMappingSrc":0x70000004C,"HIDKeyboardModifierMappingDst":0x700000039}]}'
